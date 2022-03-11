adb0f42c6981800eebbbdde991ed322cf189b6ad
LucasRavier <92583218+LucasRavier@users.noreply.github.com>
2022-03-11 10:30:13 +0100

    Add files via upload

---
+++ prey_predator/agents.py
@@ -0,0 +1,113 @@
+from mesa import Agent
+from prey_predator.random_walk import RandomWalker
+
+
+class Sheep(RandomWalker):
+    """
+    A sheep that walks around, reproduces (asexually) and gets eaten.
+
+    The init is the same as the RandomWalker.
+    """
+
+    energy = None
+
+    def __init__(self, unique_id, pos, model, moore, energy):
+        super().__init__(unique_id, pos, model, moore=moore)
+        self.energy = energy
+
+    def step(self):
+        """
+        A model step. Move, then eat grass and reproduce.
+        """
+        self.random_move()
+        self.energy -= 1
+        alive = True
+
+        if self.model.grass: 
+
+            # Eating
+            cell = self.model.grid.get_cell_list_contents([self.pos])
+            grass_patch = [obj for obj in cell if isinstance(obj, GrassPatch)][0]
+            if grass_patch.fully_grown:
+                self.energy += self.model.sheep_gain_from_food
+                grass_patch.fully_grown = False
+
+            # Dying
+            if self.energy < 0:
+                self.model.grid._remove_agent(self.pos, self)
+                self.model.schedule.remove(self)
+                alive = False
+
+        if alive and self.random.random() < self.model.sheep_reproduce:
+            # Reproducing:
+            baby = Sheep(self.model.next_id(), self.pos, self.model, self.moore, self.energy)
+            self.model.grid.place_agent(baby, self.pos)
+            self.model.schedule.add(baby)
+        
+
+
+class Wolf(RandomWalker):
+    """
+    A wolf that walks around, reproduces (asexually) and eats sheep.
+    """
+
+    energy = None
+
+    def __init__(self, unique_id, pos, model, moore, energy=None):
+        super().__init__(unique_id, pos, model, moore=moore)
+        self.energy = energy
+
+    def step(self):
+        self.random_move()
+        self.energy -= 1
+
+        # Eating
+        cell = self.model.grid.get_cell_list_contents([self.pos])
+        sheep = [obj for obj in cell if isinstance(obj, Sheep)]
+        if len(sheep) > 0:
+            sheep_to_eat = self.random.choice(sheep)
+            self.energy += self.model.wolf_gain_from_food
+            self.model.grid._remove_agent(self.pos, sheep_to_eat)
+            self.model.schedule.remove(sheep_to_eat)
+
+        # Dying
+        if self.energy < 0:
+            self.model.grid._remove_agent(self.pos, self)
+            self.model.schedule.remove(self)
+        # Reproducing
+        else:
+            if self.random.random() < self.model.wolf_reproduce:
+                baby = Wolf(self.model.next_id(), self.pos, self.model, self.moore, self.energy)
+                self.model.grid.place_agent(baby, baby.pos)
+                self.model.schedule.add(self, baby)
+
+
+class GrassPatch(Agent):
+    """
+    A patch of grass that grows at a fixed rate and it is eaten by sheep
+    """
+
+    def __init__(self, unique_id, pos, model, fully_grown, countdown):
+        """
+        Creates a new patch of grass
+
+        Args:
+            grown: (boolean) Whether the patch of grass is fully grown or not
+            countdown: Time for the patch of grass to be fully grown again
+        """
+        super().__init__(unique_id, model)
+
+        self.fully_grown = fully_grown
+        self.countdown = countdown
+        self.pos = pos
+
+    def step(self):
+
+        if not self.fully_grown:
+            if self.countdown > 0:
+                self.countdown -= 1     
+            else:
+                self.fully_grown = True
+                self.countdown = self.model.grass_regrowth_time
+                
+
---
+++ prey_predator/server.py
@@ -0,0 +1,76 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.UserParam import UserSettableParameter
+
+from prey_predator.agents import Wolf, Sheep, GrassPatch
+from prey_predator.model import WolfSheep
+
+
+def wolf_sheep_portrayal(agent):
+
+    portrayal = {"Filled": "true"}
+
+    if type(agent) is Sheep:
+
+        portrayal["Color"] = "#FFFFFF"
+        portrayal["Shape"] = "circle"
+        portrayal["r"] = 0.5
+        portrayal["Layer"] = 1
+
+    elif type(agent) is Wolf:
+
+        portrayal["Color"] = "#f00020"
+        portrayal["Shape"] = "rect"
+        portrayal["w"] = 0.5
+        portrayal["h"] = 0.5
+        portrayal["Layer"] = 2
+        
+
+    elif type(agent) is GrassPatch:
+        if agent.fully_grown:
+            portrayal["Color"] = "#008000"
+        else:
+            portrayal["Color"] = "#98FB98"
+        portrayal["Shape"] = "rect"
+        portrayal["w"] = 0.9
+        portrayal["h"] = 0.9
+        portrayal["Layer"] = 0
+
+    return portrayal
+
+
+canvas_element = CanvasGrid(wolf_sheep_portrayal, 20, 20, 500, 500)
+chart_element = ChartModule(
+    [{"Label": "Wolves", "Color": "#f00020"}, {"Label": "Sheep", "Color": "#000000"}]
+)
+
+model_params = {
+
+    "grass": UserSettableParameter("checkbox", "Grass Enabled", True),
+    "grass_regrowth_time": UserSettableParameter(
+        "slider", "Grass Regrowth Time", 10, 1, 20
+    ),
+    "initial_sheep": UserSettableParameter(
+        "slider", "Initial Sheep Population", 100, 1, 200
+    ),
+    "sheep_reproduce": UserSettableParameter(
+        "slider", "Sheep Reproduction Rate", 0.05, 0.01, 1.0, 0.01
+    ),
+    "initial_wolves": UserSettableParameter(
+        "slider", "Initial Wolf Population", 10, 1, 100
+    ),
+    "wolf_reproduce": UserSettableParameter(
+        "slider", "Wolf Reproduction Rate", 0.05, 0.01, 1.0, 0.01,
+    ),
+    "wolf_gain_from_food": UserSettableParameter(
+        "slider", "Wolf Gain From Food Rate", 25, 1, 50
+    ),
+    "sheep_gain_from_food": UserSettableParameter(
+        "slider", "Sheep Gain From Food", 5, 1, 10
+    ),
+}
+
+server = ModularServer(
+    WolfSheep, [canvas_element, chart_element], "Prey Predator Model", model_params
+)
+server.port = 8521
